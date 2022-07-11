a993936c7a30ff82776c18195df9ffc6dbbab6ab
RightSoft2020 <right.soft@yahoo.com>
2022-07-11 22:48:18 +0400

    create sample repository for DI

---
+++ src/main/java/com/developerhouse/dhsdi/DhsDiApplication.java
@@ -0,0 +1,13 @@
+package com.developerhouse.dhsdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhsDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DhsDiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/developerhouse/dhsdi/DhsDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developerhouse.dhsdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhsDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
